1a54b57fbf16bbe985d252802cbdc192edfd41e1
Ivan Pavluchuk <java.python10@gmail.com>
2023-05-16 00:57:01 +0300

    lesson_22

--- src/schoolOnline/entitiesUtils/LogUtil.java
+++ src/schoolOnline/entitiesUtils/LogUtil.java
@@ -6,7 +6,10 @@ import schoolOnline.entities.Log;
 import java.util.ArrayList;
 
 public class LogUtil {
-    static ArrayList<Log> logArr = new ArrayList<>();
+    private static ArrayList<Log> logArr = new ArrayList<>();
+    public static ArrayList<Log> getLogs(){
+        return logArr;
+    }
     public static String create(String className, int level){
         Log log = new Log(className, level);
         logArr.add(log);
